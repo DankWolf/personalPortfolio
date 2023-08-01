a3ecc0f31f8e6843708bcdc79f6dd71e0fabe27d
Christopher <chrisatenorio@gmail.com>
2023-07-31 22:01:44 -0700

    fake content to be mapped

--- src/data.js
+++ src/data.js
@@ -12,23 +12,155 @@ export const featuredPorfolio = [
     img: "",
   },
   {
-    id: 1,
+    id: 3,
     title: "E-commerce Web Design",
     img: "",
   },
   {
-    id: 1,
+    id: 4,
     title: "Social Media App",
     img: "",
   },
   {
+    id: 5,
+    title: "Hero Web Design",
+    img: "",
+  },
+  {
+    id: 6,
+    title: "Banking App",
+    img: "",
+  },
+];
+
+export const webPortfolio = [
+  {
     id: 1,
+    title: "Web Social Media App",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Web Rampa UI Design",
+    img: "",
+  },
+  {
+    id: 3,
+    title: "E-commerce Web Design",
+    img: "",
+  },
+  {
+    id: 4,
     title: "Social Media App",
     img: "",
   },
   {
+    id: 5,
+    title: "Hero Web Design",
+    img: "",
+  },
+  {
+    id: 6,
+    title: "Banking App",
+    img: "",
+  },
+];
+
+export const mobile Porfolio = [
+  {
     id: 1,
-    title: "Social Media App",
+    title: "Mobile Social Media App",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Mobile Rampa UI Design",
+    img: "",
+  },
+  {
+    id: 3,
+    title: "Mobile E-commerce Web Design",
+    img: "",
+  },
+  {
+    id: 4,
+    title: "Mobile Social Media App",
+    img: "",
+  },
+  {
+    id: 5,
+    title: "Mobile Hero Web Design",
+    img: "",
+  },
+  {
+    id: 6,
+    title: "Mobile Banking App",
+    img: "",
+  },
+];
+
+export const designPorfolio = [
+  {
+    id: 1,
+    title: "Design Social Media App",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Design Rampa UI Design",
+    img: "",
+  },
+  {
+    id: 3,
+    title: "Desgin E-commerce Web Design",
+    img: "",
+  },
+  {
+    id: 4,
+    title: "Design Social Media App",
+    img: "",
+  },
+  {
+    id: 5,
+    title: "Design Hero Web Design",
+    img: "",
+  },
+  {
+    id: 6,
+    title: "Design Banking App",
+    img: "",
+  },
+];
+
+export const contentPorfolio = [
+  {
+    id: 1,
+    title: "Content Social Media App",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Content Rampa UI Design",
+    img: "",
+  },
+  {
+    id: 3,
+    title: "Content E-commerce Web Design",
+    img: "",
+  },
+  {
+    id: 4,
+    title: "Content Social Media App",
+    img: "",
+  },
+  {
+    id: 5,
+    title: "Content Hero Web Design",
+    img: "",
+  },
+  {
+    id: 6,
+    title: "Content Banking App",
     img: "",
   },
 ];
